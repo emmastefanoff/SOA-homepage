796206c92b2d3f14c6a98c90f6f9d12ad0fdd432
Emma <es068015@ohio.edu>
2019-04-23 18:29:49 -0400

    carousel for teams

--- js/index.js
+++ js/index.js
@@ -2,3 +2,15 @@
 $('.handle').on('click', function() {
   $('nav ul').toggleClass('showing')
 });
+
+$(".carousel").swipe({
+
+  swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+
+    if (direction == 'left') $(this).carousel('next');
+    if (direction == 'right') $(this).carousel('prev');
+
+  },
+  allowPageScroll:"vertical"
+
+});
